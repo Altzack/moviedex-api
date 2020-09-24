ea2d2c729416ea4b083a7b2deeb011ebd83ce49c
Zack <altschulerzack@yahoo.com>
2020-09-24 14:59:34 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIEDEX = require("./moviedex.json");
+
+const app = express();
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use(function handleBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "unauthorized" });
+  }
+  next();
+});
+
+function handleGetMovies(req, res) {
+  let response = MOVIEDEX.movies;
+
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+}
+
+app.get("/movie", handleGetMovies);
+
+const PORT = 7000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
